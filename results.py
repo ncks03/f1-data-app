5bc3e9cc804367ce72186c645b982bccffff9d17
ncks03 <nick@schuitenco.nl>
2024-09-09 13:12:13 +0200

    Implemented results API Request

--- results.py
+++ results.py
@@ -2,4 +2,9 @@ import requests
 
 def get_results():
 
-    race_results = requests.get('http://ergast.com/api/f1/current/last/results')
+    race_results = requests.get('http://ergast.com/api/f1/current/last/results.json')
+
+    print(race_results.json())
+
+if __name__ == '__main__':
+    get_results()
