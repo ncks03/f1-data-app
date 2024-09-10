81ee3fb169f26eb52172d6946f10ff649f08bc54
ncks03 <nick@schuitenco.nl>
2024-09-10 12:34:26 +0200

    more work on schedule page

--- race_schedule.py
+++ race_schedule.py
@@ -2,6 +2,7 @@ import requests
 
 def fetch_race_schedule(season):
 
+    # Fetch schedule from ergast API
     race_schedule = requests.get(f'http://ergast.com/api/f1/{season}')
 
     return race_schedule.json()
@@ -10,5 +11,9 @@ def display_race_schedule():
 
     # Ask user which year's schedule they want to view
     year_choice = input('For which year would you like to see the race schedule?\n')
-    race_schedule_dict = fetch_race_schedule(year_choice)
+    race_schedule_dict = fetch_race_schedule(year_choice).json()
+
+    # Convert race schedule to usable dictionaries
+
+
 
